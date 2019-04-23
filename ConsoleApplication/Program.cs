fa62eec1e7839f0e49d511c20203bc3af27df09d
izzysilv <35540604+izzysilv@users.noreply.github.com>
2019-04-23 11:27:00 -0600

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Israel Silva";
+            var location = "Idaho";
+
+
+        }
+    }
+}
